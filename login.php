851bc1708c0f82f0f9a1ddcb9e238343ba43e129
Saw-hash <saw.gwaw@gmail.com>
2020-11-15 17:44:47 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,16 +1,22 @@
-<?php include('server.php') ?>
+<?php  header("Access-Control-Allow-Origin: *"); 
+
+
+//header("Access-Control-Allow-Origin: http://localhost:70800");
+include('server.php'); ?>
 
 <!DOCTYPE html>
 <html>
 
 <head>
-    <meta charset="utf-8" />
+   <meta content="text/html; charset=UTF-8; X-Content-Type-Options:nosniff"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>#1 HTML Login Form</title>
+    <meta charset="utf-8" />
+    
+    <title>Login Form</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link type="text/css" rel="stylesheet" href="main.css">
-  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet">  
+   <link type="text/css" rel="stylesheet" href="main.css"> 
+  <link type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet">
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
@@ -55,7 +61,7 @@
             <img src="sea.png" alt="sea store">
         </div> 
         
-        <form id="form" method="post" action="login.php">
+        <form id="form1" method="post" action="login.php">
         <div id="form_right">
             <?php include('errors.php'); ?>
             
@@ -64,11 +70,11 @@
                 <i class="fas fa-envelope"></i>
                
                 <input placeholder="Username" type="text" name="username" id="username" class='input_field' >
-                <label for="username"> </label>
+               <span id="$username_err"> </span>
                
             </div>
-            <label for="username"> <span></span></label>
-             <div id="error"> </div>
+            
+            
             <div class="input_container">
                 <i class="fas fa-lock"></i>
                 
@@ -77,7 +83,7 @@
             
             
             
-            <input type="submit" value="Login" id='input_submit' class='input_field'>
+            <input name="login_user" type="submit" value="Login" id='input_submit' class='input_field'>
             
             <span>Forgot <a href="#"> Username / Password ?</a></span>
             
