0f752757187adfa9561a8f6967232e84af449dfe
Saw-hash <saw.gwaw@gmail.com>
2020-11-16 10:22:53 +0800

    Update Product_Search.php

--- Product_Search.php
+++ Product_Search.php
@@ -1,64 +1,84 @@
 <!DOCTYPE html>
 <html>
-<head> 
-<title> Product Search </title> 
+<head><title> table</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     
-    <script>
-function table() {
+    <link rel="stylesheet" type="text/css" href="style_product.css">
     
-  var x = document.getElementById("myDIV");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-   x.style.display = "none";
-  }
-}
-</script>
-  </head>
-
-<body>
-  
-    <h1> Product Search Result</h1>
-    
-  <?php include('config.php'); ?>
-
-<?php
-
-    $host = "localhost";
-    $dbuser = "root";
-    $dbpass = "";
-    $dbase = "X33577055";
-    error_log("test error");
-   $conn = new mysqli($host,$dbuser,$dbpass,$dbase);
-
-
-     // Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+    <link rel="stylesheet" href="css/Style.css">
     
     
-
- if(isset($_POST['search'])) {
+    
+    <!-------Js for toggle menu-------->
+  <!--  <script>
+        var MenuItems = document.getElementById("MenuItems");
+        
+        MenuItems.style.maxHeight = "0px";
+        
+        function menutoggle(){
+            if(MenuItems.style.maxHeight == "0px")
+            {
+                MenuItems.style.maxHeight = "200px"
+            }
+            else
+            {
+                MenuItems.style.maxHeight = "0px"
+            }
+        }
+        
+    </script>-->
      
-     $search_input = $_POST['search_input'];
-     //$lname = $_POST['lname'];
-     $query = "SELECT * FROM Product where 
-     (ProductID = '$search_input') 
-     OR (ProductName = '$search_input') 
-     OR (ProductType = '$search_input')
-     OR (ProductBrand = '$search_input')
-     OR (ProductDescription = '$search_input')
-     OR (ProductPrice = '$search_input')";
-     
-     $query_run = mysqli_query($conn, $query);
+</head>
+
+  <body>  
+      <table class='table'> 
+      <div class="header">
+        <div class="container">
+            <div class="navbar">
+            <div class="logo"> 
+            <img src="images/Logo.png" width="125px">
+            </div>
+                
+            <nav>
+                <ul id="MenuItems">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="products.html">Products</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                    <li><a href="">Account</a></li>
+                    <li><div class="search-container">
+                        
+                        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" >
+                            <input type="text" name="search_input" id="search_input">
+                                <input type="submit" name="search" id="search" value="Search"/>
+                        </form>
+                        
+                       <!-- <form action="/action_page.php">
+                          <input type="text" placeholder="Search.." name="search">
+                          <button type="submit"><i class="fa fa-search"></i>
+                            </button>
+                        </form>-->
+           
+                      </div>
+                    </li>
+                </ul>
+            </nav>
+                
+                 <img src="images/cart.png" width="30px" height="30px">
+                <img src="images/icons8-menu-30.png" class="menu-icon" onclick="menutoggle()"> 
+            </div>
+        </div>
+    </div>
+      
+      </table>
+      
+
+      <!--SQL query table start-->
+ 
+    <table class='table' id='search' border='0'>
     
-    
- ?>   
-<table id='search' border='0' style="color: blue" >
-   <?php echo "
-    
-    <tr>
+    <thead>
     <th>ProductID</th>
     <th>ProductName</th>
     <th>ProductType</th>
@@ -66,28 +86,155 @@ if ($conn->connect_error) {
     <th>ProductDescription</th>
     <th>ProductImage</th>
     <th>ProductPrice</th>
-    </tr>";
+    </thead>
+      
+    <?php include('config.php'); 
+        
+        //validate search
+            if ((isset($_REQUEST['search'])) && (empty($_REQUEST['search'])))
+            {  die("Please type input");   }
+
+                if(isset($_POST['search'])) {
+                   if (empty($_REQUEST['search'])){
+                      die ("empy input");
+                  }
+
+                    
+                
+                 $search_input = $_POST['search_input'];
+                $query = "SELECT * FROM product where 
+                    ProductID LIKE '%{$search_input}%' 
+                OR  ProductType LIKE '%{$search_input}%'
+                OR  ProductBrand LIKE '%{$search_input}%'
+                OR  ProductType LIKE '%{$search_input}%'
+                OR  ProductDescription LIKE '%{$search_input}%'
+                OR  ProductPrice LIKE '%{$search_input}%'
+
+                ";
+
+                    $query_run = mysqli_query($conn, $query);
+    
+                
+        /*display search query table*/
           
-  //while($row = $result->fetch_assoc()) {
-   while($row = mysqli_fetch_array($query_run))  {
-         
+        while($row = $query_run->fetch_assoc()) {
+ //  while($row = mysqli_fetch_array($query_run))  {
+      echo "<tbody>";
       echo "<tr>";
-      echo "<td>" . $row['ProductID'] . "</td>";
-      echo "<td>" . $row['ProductName'] . "</td>";
-      echo "<td>" . $row['ProductType'] . "</td>";
-      echo "<td>" . $row['ProductBrand'] . "</td>";
-      echo "<td>" . $row['ProductDescription'] . "</td>";
-      echo "<td>" . $row['ProductImage'] . "</td>";
-      echo "<td>" . "$" . $row['ProductPrice'] . "</td>";
-    echo "</tr>"; 
+      echo "<td data-label='ProductID'>" . $row['ProductID'] . "</td>";
+      echo "<td data-label='ProductName'>" . $row['ProductName'] . "</td>";
+      echo "<td data-label='ProductType'>" . $row['ProductType'] . "</td>";
+      echo "<td data-label='ProductBrand'>" . $row['ProductBrand'] . "</td>";
+      echo "<td data-label='ProductDescription'>" . $row['ProductDescription'] . "</td>";
+      echo "<td data-label='ProductImage'>" . $row['ProductImage'] . "</td>";
+      echo "<td data-label='ProductPrice'>" . "$" . $row['ProductPrice'] . "</td>";
+    echo "</tr>";
+      
+    echo "</tbody>";
   }
     
 } else {
   echo "0 results";
-} 
+        
+}    
+ 
+ mysqli_close($conn);
     ?>
-    </table>
+        
+      </table>
+      
+      
+      <!--display all-->
+      
+      <?php 
     
+ /*
+      
+      $sql = "SELECT * FROM product";
+
+$result = $conn->query($sql);
+//$result = mysqli_query($conn, $sql);
+      
+if ($result->num_rows > 0) {
+  // output data of each row
+   echo "<table id='search' border='0'>
+    
+    <thead>
+    <th>ProductID</th>
+    <th>ProductName</th>
+    <th>ProductType</th>
+    <th>ProductBrand</th>
+    <th>ProductDescription</th>
+    <th>ProductImage</th>
+    <th>ProductPrice</th>
+    </thead> ";
+          
+  while($row = $result->fetch_assoc()) {
+ //  while($row = mysqli_fetch_array($query_run))  {
+      echo "<tbody>";
+      echo "<tr>";
+      echo "<td data-label='ProductID'>" . $row['ProductID'] . "</td>";
+      echo "<td data-label='ProductName'>" . $row['ProductName'] . "</td>";
+      echo "<td data-label='ProductType'>" . $row['ProductType'] . "</td>";
+      echo "<td data-label='ProductBrand'>" . $row['ProductBrand'] . "</td>";
+      echo "<td data-label='ProductDescription'>" . $row['ProductDescription'] . "</td>";
+      echo "<td data-label='ProductImage'>" . $row['ProductImage'] . "</td>";
+      echo "<td data-label='ProductPrice'>" . "$" . $row['ProductPrice'] . "</td>";
+    echo "</tr>";
+      
+    echo "</tbody>";
+
+} //else {
+ // echo "0 results";
+}
+    
+$conn->close();
+      
+     */ ?>
+      
+      
+      
+      
+      <!-- Footer -->
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col-1">
+                    <h3>Download our App</h3>
+                    <p>Download App for android and ios mobile phone</p>
+                    <div class="app-logo">
+                        <img src="images/google-play-badge.png">
+                        <img src="images/App_Store_Badge.png">
+                    </div>
+                </div>
+                <div class="footer-col-2">
+                    <img src="images/Logo.png">
+                    <p>Our Purpose is to sustainably make the pleasure and usefulness of home appliances to the many</p>
+                </div>
+                <div class="footer-col-3">
+                    <h3>Useful Links</h3>
+                    <ul>
+                        <li>Coupons</li>
+                        <li>Blog Post</li>
+                        <li>Return Policy</li>
+                        <li>Join Affiliate</li>
+                    </ul>
+                </div>
+                <div class="footer-col-4">
+                    <h3>Follow Us</h3>
+                    <ul>
+                        <li>Facebook</li>
+                        <li>Twitter</li>
+                        <li>Instagram</li>
+                        <li>YouTube</li>
+                    </ul>
+                </div>
+            </div>
+            <hr>
+            <p class="copyright">Copyright 2020 - SEA Store</p>
+        </div>
+    </div>
+       
 </body>
 </html>
 
