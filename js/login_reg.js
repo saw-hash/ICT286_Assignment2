273a37a2c95041de00cba3dfd3a5d4b92fb5b807
Saw-hash <saw.gwaw@gmail.com>
2020-11-17 21:25:04 +0800

    Create login_reg.js

---
+++ js/login_reg.js
@@ -0,0 +1,95 @@
+/*$(function(){
+    var $registerForm = $("registration);
+        if($registerForm)")
+});*/
+
+$(document).ready(function() {
+
+    $("#myform").validate({
+    
+     rules: {
+         username: {
+             required: true,
+             minlength: 5
+         },
+         
+        custname: {
+             required: true,
+             minlength: 5
+         },
+         
+         email: {
+             required: true
+            
+         },
+         
+        phone: {
+             required: true,
+             minlength: 8,
+             maxlength: 10
+         },
+    
+         
+        country:{ 
+             required: true,
+         },
+         
+        subject:{ 
+             required: true,
+         },
+            
+        password: {
+             required: true,
+            minlength: 5
+        },
+        confirmpassword: {
+            required: true,
+            //equalTo: "#password"
+           
+            //passwordMatch: true // set this on the field you're trying to match
+        },
+         
+        
+     },
+     messages:{
+         username: {
+             required: "Name is required...",
+             minlength: "enter number and character eg.user1",
+         },
+         
+            custname: {
+             required: "Name is required...",
+            
+         },
+         
+            email: {
+                required:"enter youe email address... eg. john@xxxmail.com"
+            
+         },
+         
+             phone: {
+                required: "plese enter your valid phone number",
+                minlength: "enter valid phone number ",
+                maxlength: "number doesn't exit"
+         },
+         
+             country: {
+                required: "Please select a country..."  
+         },
+         
+  
+            password: {
+                required: "Password is required...",
+                minlength: "enter number and character eg.user1",
+         },
+         
+            confirmpassword: {
+                required: "This is required",
+                equalTo: "Passwords do not match!",
+            
+                minlength: "enter number and character eg.user1",
+         }
+     }   
+       
+ });
+});  
